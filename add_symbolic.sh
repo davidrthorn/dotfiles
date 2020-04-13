c67284ce21faeb1fc78f12a8311d4a13e259604a
David Rowthorn <davidrowthorn@gmail.com>
2020-04-13 09:29:03 +0100

    added shell script for mac

--- add_symbolic.sh
+++ add_symbolic.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/init.vim ~/.config/nvim
+ln -s ~/.dotfiles/settings.mac.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/.dotfiles/workmac.zshrc ~/.zshrc
